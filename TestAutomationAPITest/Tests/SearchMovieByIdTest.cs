6ce5048dc5386059cc64ec4f31de4fc271352410
brkmck <brkmck1@gmail.com>
2021-01-18 01:32:16 +0300

    Revert "Revert "Proje dosyası ekle.""
    This reverts commit e2b0adf74fca4b57a82baec23d91e2221093efb4.

---
+++ TestAutomationAPITest/Tests/SearchMovieByIdTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using TestAutomationAPITest.Models;
+
+namespace TestAutomationAPITest.Tests
+{
+    public class SearchMovieByIdTest
+    {
+        private const string EXPECTED_IMDB_ID = "tt0241527";
+        private const string EXPECTED_TITLE = "Harry Potter and the Sorcerer's Stone";
+        private const string EXPECTED_YEAR = "2001";
+        private const string EXPECTED_RELEASED = "16 Nov 2001";
+
+        [Test]
+        public void MovieTest()
+        {
+            SearchMovieByIdModel model = new SearchMovieByIdModel();
+            const string searchMovie = "Harry Potter";
+            const string movieName = "Harry Potter and the Sorcerer's Stone";
+
+            //Get movie id by search
+            string movieImdbId = model.GetMovieIDBySearch(searchMovie, movieName);
+            Assert.AreEqual(EXPECTED_IMDB_ID, movieImdbId);
+
+            //Get movie infos by id
+            Dictionary<string, string> movieInfo = model.GetMovieInfosById(movieImdbId);
+            string movieTitle = movieInfo["Title"];
+            string movieYear = movieInfo["Year"];
+            string movieReleased = movieInfo["Released"];
+
+            Assert.AreEqual(EXPECTED_TITLE, movieTitle);
+            Assert.AreEqual(EXPECTED_YEAR, movieYear);
+            Assert.AreEqual(EXPECTED_RELEASED, movieReleased);
+        }
+    }
+}
